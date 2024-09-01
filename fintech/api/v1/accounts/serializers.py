0b1e8869bfd3a03ca928011d4b2313f4d87ff961
AcidInvader <124760286+AcidInvader@users.noreply.github.com>
2024-09-01 16:15:29 +0300

    created accounts logic

--- fintech/api/v1/accounts/serializers.py
+++ fintech/api/v1/accounts/serializers.py
@@ -3,23 +3,41 @@ from accounts.models import Account, AccountTransaction
 
 
 class AccountSerializer(serializers.ModelSerializer):
+    """ Сериализатор для создания счета. """
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     class Meta:
         model = Account
-        fields = ['id', 'number', 'balance', 'currency', ]
-
-
-# class AccountTransactionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = AccountTransaction
-#         fields = '__all__'
-
-class TopUpBalanceSerializer(serializers.Serializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
-
+        fields = ['id', 'number', 'amount', 'currency']
 
     def validate_amount(self, value):
-        if value <= 0:
+        print(f'{value=}')
+        if value < 0:
+            raise serializers.ValidationError("Amount must be positive.")
+        return value
+
+
+class TopUpAccountSerializer(serializers.Serializer):
+    """ Сериализатор для пополнения баланса счета. """
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    account_number = serializers.CharField()
+
+    def validate_amount(self, value):
+        print(f'{value=}')
+        if value < 0:
             raise serializers.ValidationError("Top-up amount must be greater than zero.")
         return value
+
+
+class WithdrawAccountSerializer(TopUpAccountSerializer):
+    """ Сериализатор для дебетования счета. """
+
+
+class AccountTransactionSerializer(serializers.ModelSerializer):
+    account_number = serializers.SerializerMethodField()
+    class Meta:
+        model = AccountTransaction
+        fields = ['id', 'data', 'type', 'amount', 'account_number']
+
+    def get_account_number(self, obj):
+        return obj.account.number
+
--- fintech/api/v1/accounts/views.py
+++ fintech/api/v1/accounts/views.py
@@ -1,22 +1,58 @@
 from rest_framework.views import APIView
 from accounts.models import Account, AccountTransaction
 from rest_framework.permissions import AllowAny
-from .serializers import AccountSerializer, TopUpBalanceSerializer
+from .serializers import AccountSerializer, TopUpAccountSerializer, WithdrawAccountSerializer, \
+    AccountTransactionSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
+
+
+class CreateAccountView(APIView):
+    permission_classes = (AllowAny,)
+
+    @extend_schema(
+        request=AccountSerializer,
+        responses={200: 'Success'}
+    )
+    def post(self, request):
+        serializer = AccountSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            account_number = serializer.validated_data['number']
+            currency = serializer.validated_data['currency']
+            amount = float(serializer.validated_data['amount'])
+            account = Account()
+            account.number = account_number
+            account.currency = currency
+            account.balance += amount
+            account.save()
+
+            return Response(
+                {'account_number': account.number, 'account_balance': account.balance, 'currency': account.currency},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TopUpView(APIView):
     permission_classes = (AllowAny,)
 
+    @extend_schema(
+        request=TopUpAccountSerializer,
+        responses={200: 'Success'},
+    )
     def post(self, request):
-        serializer = TopUpBalanceSerializer(data=request.data)
+        serializer = TopUpAccountSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
             account_number = serializer.validated_data['account_number']
             amount = serializer.validated_data['amount']
             account = get_object_or_404(Account, number=account_number)
+            print(f'{amount=}')
 
             account.balance += amount
             account.save()
@@ -27,7 +63,82 @@ class TopUpView(APIView):
                 amount=amount,
             )
 
-            return Response({'balance': account.balance}, status=status.HTTP_200_OK)
+            return Response({'account_number': account.number,
+                             'account_balance': account.balance,
+                             'currency': account.currency},
+                            status=status.HTTP_200_OK
+                            )
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class WithdrawAccountView(APIView):
+    permission_classes = (AllowAny,)
+
+    @extend_schema(
+        request=WithdrawAccountSerializer,
+        responses={200, 'success'}
+    )
+    def post(self, request):
+        serializer = WithdrawAccountSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            print(f'we are in if')
+            account_number = serializer.validated_data['account_number']
+            amount = serializer.validated_data['amount']
+            print(f'{amount=}')
+            account = get_object_or_404(Account, number=account_number)
+            print(f'{account=}')
+            if account.balance >= amount:
+                account.balance -= amount
+                account.save()
+
+                AccountTransaction.objects.create(
+                    account=account,
+                    type=AccountTransaction.Type.WITHDRAW,
+                    amount=amount,
+                )
+
+                return Response({'account_number': account.number,
+                                 'account_balance': account.balance,
+                                 'currency': account.currency},
+                                status=status.HTTP_200_OK)
+            else:
+                return Response({'detail': 'The account balance less then amount of withdraw'},
+                                status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+class AccountTransactionView(APIView):
+    permission_classes = (AllowAny,)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='account-number',
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.HEADER,
+                description='Account number to retrieve transactions for it',
+                required=True,
+            ),
+        ],
+        responses={200: AccountTransactionSerializer(many=True)}
+    )
+    def get(self, request):
+        try:
+            print(f'{request=}')
+            account_number = request.headers.get('account_number')
+            if not account_number:
+                return Response({"error": "Account Number is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+            account = get_object_or_404(Account, number=account_number)
+            transactions = AccountTransaction.objects.filter(account=account)
+
+            serializer = AccountTransactionSerializer(transactions, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Exception as ex:
+            return ex
+
+
+
 
